a9b640ab0a62cca4201bacde31b8126f03c2fb98
Santi Racca <santiagoracca7@gmail.com>
2024-05-27 14:30:59 -0600

    task group

--- coroutine.py
+++ coroutine.py
@@ -9,7 +9,14 @@ async def fetch_data(id, delay):
 
 ## coroutine function
 async def main():
-  results = await asyncio.gather(fetch_data(1, 2), fetch_data(2, 1), fetch_data(3, 3))
+  tasks = []
+
+  async with asyncio.TaskGroup() as tg:
+    for i, sleep_time in enumerate([2,1,3], start=1):
+      task = tg.create_task(fetch_data(i, sleep_time))
+      tasks.append(task)
+
+  results = [task.result() for task in tasks]
 
   for result in results:
     print(f"Result: {result}")
