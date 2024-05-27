07472abb571cd342e6b9216535d5bf3e79c2275a
Santi Racca <santiagoracca7@gmail.com>
2024-05-27 14:28:11 -0600

    gather function

--- coroutine.py
+++ coroutine.py
@@ -9,16 +9,12 @@ async def fetch_data(id, delay):
 
 ## coroutine function
 async def main():
-  task1 = asyncio.create_task(fetch_data(1, 2))
-  task2 = asyncio.create_task(fetch_data(2, 1))
-  task3 = asyncio.create_task(fetch_data(3, 3))
+  results = await asyncio.gather(fetch_data(1, 2), fetch_data(2, 1), fetch_data(3, 3))
 
-  result1 = await task1
-  result2 = await task2
-  result3 = await task3
+  for result in results:
+    print(f"Result: {result}")
 
 
-  print(result1, result2, result3)
 
 
 
