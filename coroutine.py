f856bcf75ef1dce22d5314700b7c80491d7b2c86
Santi Racca <santiagoracca7@gmail.com>
2024-05-27 14:14:50 -0600

    coroutines

--- coroutine.py
+++ coroutine.py
@@ -10,11 +10,13 @@ async def fetch_data(delay):
 
 ## coroutine function
 async def main():
-  print('Start of main coroutine')
-  task = fetch_data(2);
-  result = await task
-  print(f'Result: {result}')
-  print('End of main coroutine')
+  task1 = fetch_data(2)
+  task2 = fetch_data(3)
+  result1 = await task1
+  print(f"Task 1 result: {result1}")
+  result2 = await task2
+  print(f"Task 2 result: {result2}")
+  
 
 
 
